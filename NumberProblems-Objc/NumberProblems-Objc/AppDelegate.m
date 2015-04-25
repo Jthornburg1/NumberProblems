1baecd2aa4818b4ab140e4dee0911757aec4d003
Jon Thornburg <reliable2192@gmail.com>
2015-04-25 14:00:43 -0600

    finished

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "AppDelegate.h"
 
+static int multiply = 8;
+static int divisor = 4;
+
 @interface AppDelegate ()
 
 @end
@@ -15,12 +18,40 @@
 @implementation AppDelegate
             
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
 
-    // Insert code here...
+    [self iterateCount: 4];
     
     
     return YES;
 }
+
+- (void) iterateCount:(int)number
+{
+   int multiplyTotal = [self multiply:number];
+    NSLog(@"Multiplied %d by %d is equal to %d", number, multiply, multiplyTotal);
+    
+    float dividedTotal = [self divided:number];
+    NSLog(@"Divided %d by %d is equal to %f", number, divisor, dividedTotal);
+    
+    if (number > 0) {
+        NSLog(@"Iterate %d", number);
+        number --;
+        [self iterateCount:number];
+        
+    }
+}
+
+- (int) multiply:(int)numCalc
+{
+    return numCalc * multiply;
+}
+
+- (float) divided:(int)numDiv
+{
+    return numDiv / divisor;
+}
+
 
 @end
